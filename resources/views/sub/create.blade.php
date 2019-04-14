622b247ad049d6144f80b378f1fab38448fbd2fa
Derrick Noutais <derricknoutais@gmail.com>
2019-04-14 22:32:16 +0100

    first commit

--- resources/views/sub/create.blade.php
+++ resources/views/sub/create.blade.php
@@ -6,6 +6,7 @@
         <div class='container'>
             <div class="row">
                 <div class="col-9">
+                    <input class="form-control">
                     <multiselect 
                         v-model="selected_sub" :options="this.prod" label="name" @search-change="query" :loading="isLoading" 
                         :multiple="true" :searchable="true"  :internal-search="false" :clear-on-select="false" :close-on-select="false" 
